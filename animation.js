34d8508fb0d73b2dd00da594790f62682509c1e1
Arman Drismir <adrismir@gmail.com>
2023-10-16 17:30:04 -0700

    added background grid

--- animation.js
+++ animation.js
@@ -1,8 +1,35 @@
+// To do: If the last row of dots gets just cut off then it will look like there is more padding on the right and bottom.
+//        To fix this I will need to make sure that the last dot is drawn at the edge of the space like the first one
+//
+//        After that I will need to handle window resize
+
 let canvas = document.getElementById("welcome_canvas");
+
+let container = document.getElementsByTagName("header")[0];
+var w = container.clientWidth;
+var h = container.clientHeight;
+
+// Properly setting the canvas size:
+canvas.width = w;
+canvas.height = h;
+
 let ctx = canvas.getContext("2d");
 
-ctx.beginPath();
-ctx.arc(10, 10, 5, 0, Math.PI * 2);
-ctx.fillStyle = "#210033";
-ctx.fill();
-ctx.stroke();
+var padding = 100;
+
+function drawCircle(x, y) {
+    ctx.beginPath();
+    ctx.arc(x + padding, y + padding, 5, 0, Math.PI * 2);
+    ctx.fillStyle = "rgba(255, 255, 255, 0.1)";
+    ctx.fill();
+    ctx.stroke();
+}
+
+var dot_spacing = 75;
+
+for (let j = 0; j * dot_spacing + padding < h - padding; j++) {
+    for (let i = 0; i * dot_spacing + padding < w - padding; i++) {
+        drawCircle(i * dot_spacing, j * dot_spacing);
+    }
+}
+
